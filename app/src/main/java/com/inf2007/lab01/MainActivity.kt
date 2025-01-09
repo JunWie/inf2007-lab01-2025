24d48eb54f12e7c3ee4f10618eec6538cbd6ea82
junjie22 <jiel0h.00@gmail.com>
2025-01-09 14:08:29 +0800

    Fixed errors in MainActivity.kt

--- app/src/main/java/com/inf2007/lab01/MainActivity.kt
+++ app/src/main/java/com/inf2007/lab01/MainActivity.kt
@@ -49,14 +49,16 @@ fun MainScreen() {
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 UserInput(
-                    name = name,
-                    onNameChange = { name = it }
+                    name = username,
+                    onNameChange = { username = it } ,
+                    modifier = Modifier.testTag("nameInput") // Added test tag for TextField
+
                 )
 
                 Button(
                     onClick = {
                         if (username.isNotBlank()) {
-                            showGreeting = false
+                            showGreeting = true //Fixed to show when text box is filled
                         }
                     },
                     modifier = Modifier
@@ -67,11 +69,12 @@ fun MainScreen() {
                 }
 
                 if (showGreeting) {
-                    Greeeting(
+                    Greeting(
                         name = username,
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(top = 16.dp)
+                            .testTag("greetingMsg")
                     )
 
                 }
@@ -84,21 +87,21 @@ fun MainScreen() {
 fun UserInput(name: String, onNameChange: (String) -> Unit, modifier: Modifier = Modifier) {
     TextField(
         value = name,
-        onValueChange = { onNameChange(it) },
+        onValueChange = onNameChange, //Fixed to onNameChange
         label = { Text("Enter your Name") },
         modifier = modifier
             .fillMaxWidth()
-            .testTag("UserInput")
+            .testTag("nameInput") // Changed to nameInput
     )
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $username!, Welcome to InF2007!",
+        text = "Hello $name!, Welcome to INF2007!", //Caps InF2007
         modifier = Modifier
             .fillMaxWidth()
-            .testTag("greeting")
+            .testTag("greetingMsg") // Changed to greetingMsg
     )
 }
 
